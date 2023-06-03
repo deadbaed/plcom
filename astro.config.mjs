e884ab4f8d3522a8eb1b505af6d385a28f3714b0
Philippe Loctaux <p@philippeloctaux.com>
2023-06-03 15:54:20 +0200

    use deno ssr with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress()],
   compressHTML: true,
+  output: "server",
+  adapter: deno({port: 8000})
 });

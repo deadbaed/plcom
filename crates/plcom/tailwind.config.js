8830a82d7172291956049f74f8ed8726a729ee4c
Philippe Loctaux <p@philippeloctaux.com>
2024-11-19 19:55:20 +0100

    tailwind: fmt

--- crates/plcom/tailwind.config.js
+++ crates/plcom/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: { 
-      relative: true,
-      files: ["./src/**/*.rs"],
+  content: { 
+    relative: true,
+    files: ["./src/**/*.rs"],
+  },
+  theme: {
+    extend: {
+      height: {
+        almostscreen: "90vh",
+        halfscreen: "60vh",
+      }
     },
-    theme: {
-      extend: {
-        height: {
-	  almostscreen: "90vh",
-	  halfscreen: "60vh",
-        }
-      },
-    },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}

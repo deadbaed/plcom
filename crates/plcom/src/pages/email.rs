3a2ebda471782de8f951c96ef55739fb8403a254
Philippe Loctaux <p@philippeloctaux.com>
2025-07-03 22:00:01 +0200

    remove pgp key, and link to keybase account

--- crates/plcom/src/pages/email.rs
+++ crates/plcom/src/pages/email.rs
@@ -11,19 +11,5 @@ pub fn email_page() -> impl IntoAny {
                     "www at philippeloctaux dot com",
                 ), Some(Icon::Email), None).into_any()}
         </div>
-
-        <p class=tw_join!(
-            "mb-2"
-        )>
-            "If you want to encrypt your message, I have a "
-            {underline_link(Link::new(uri!("/pub/pgp-0x69771CD04BA82EC0.txt").into(), "pgp key"), None).into_any()}
-            " at your disposal."
-        </p>
-        <p class=tw_join!(
-            "mb-2"
-        )>
-            "I also have a "{underline_link(Link::new(uri!("/keybase.txt").into(), "Keybase"), None).into_any()}
-            " account, but I do not check it often."
-        </p>
     }.into_any()
 }

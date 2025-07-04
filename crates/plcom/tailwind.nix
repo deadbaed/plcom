7a6f1ee929921f7ed7dc0ca728ced524c57384d9
Philippe Loctaux <p@philippeloctaux.com>
2025-07-05 01:22:24 +0200

    tailwind.nix: tailwind cli is a build depedency, not at runtime

--- crates/plcom/tailwind.nix
+++ crates/plcom/tailwind.nix
@@ -8,7 +8,7 @@
 stdenvNoCC.mkDerivation {
   name = "plcom-css-tailwind";
   inherit src;
-  buildInputs = [ tailwindcss ];
+  nativeBuildInputs = [ tailwindcss ];
   dontUnpack = true;
   buildPhase = "${tailwindcss}/bin/tailwindcss --config ${src}/tailwind.config.js --input ${src}/${inputFile} --output $out/output.css --minify";
 }
